11085ffdb3ce3606b3d440e4f4382c6917dfbc5a
rayhm6419 <haoml6419@gmail.com>
2023-04-05 01:47:36 -0700

    navBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../style/Navbar.css"
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import '../style/Navbar.css';
+import { MenuOutlined } from '@ant-design/icons';
 
 function Navbar() {
+  //when resize the screen to 600px, nav bar become toggle bar
+  //may happen in mobile users
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => { 
+    setIsOpen(!isOpen);
+  }
+
+  //when after click the links in the toggle bar, the toggle bar would disappear
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsOpen(false)
+  },[location]);
+
   return (
-    <div className='navbar'>
-        <div className='toggleButton'>
-            <button></button>
-        </div>
-        <div className='links'>
-            <Link to="/"> Home</Link>
-            <Link to="/gallery"> Gallery</Link>
-            <Link to="/PlayGround"> PlayGround</Link>
-        </div>
+    <div className="navbar" id={isOpen ? "open" : "close"}>
+      <div className="toggleButton" onClick={toggleMenu}>
+      <MenuOutlined />
+      </div>
+      <div className='links'>
+        <Link to="/"> Home </Link>
+        <Link to="/gallery"> Gallery </Link>
+        <Link to="/playGround"> PlayGround </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
